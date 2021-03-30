72708235b27d8925611f5fefbefe350826a3f230
Sanya <sanyaboliuh@gmail.com>
2021-03-30 23:19:19 +0300

    Add exception in function (and function into class), which search element

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,3 +4,6 @@ from selenium.webdriver.common.by import By
 class MainPage(BasPage):
     def go_to_login_page(self):
         self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
